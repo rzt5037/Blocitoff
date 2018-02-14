0af434b8ecd1b2ddd8b0ae067cfea069602bc0a9
Richard Travis <r.z.travis@gmail.com>
2018-02-14 09:07:22 -0700

    User and item seeds added

--- Gemfile
+++ Gemfile
@@ -39,6 +39,7 @@ gem 'bootstrap-sass'
 group :development, :test do
   # Call 'byebug' anywhere in the code to stop execution and get a debugger console
   gem 'byebug'
+  gem 'faker'
 end
 
 group :development do
--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,26 @@
 #
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
+require 'faker'
+
+5.times do
+  User.create!(
+    email:    Faker::Internet.unique.email,
+    password: Faker::Internet.password(8)
+  )
+end
+users = User.all
+
+
+15.times do
+  Item.create!(
+    user:     users.sample,
+    name:     Faker::Science.element
+  )
+end
+item = Item.all
+
+
+puts "Seed finished"
+puts "#{User.count} users created"
+puts "#{Item.count} items created"
